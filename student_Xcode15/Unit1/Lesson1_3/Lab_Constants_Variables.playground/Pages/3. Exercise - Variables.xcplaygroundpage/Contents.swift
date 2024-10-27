fd3d4d705fb40697d4be43e7e1857b768e5c4223
Sawyerj93 <hrlyswyr@gmail.com>
2024-10-27 09:44:00 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -3,13 +3,15 @@
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
-
+var schooling = 8
+print(schooling)
 
 //:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
-
+schooling = 9
+print(schooling)
 
 //:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
-
+print("The above code does compile because schooling is assigned as a variable that can change. by simply doing schooling = 9 it changes the previous variable to the current variable")
 
 /*:
 [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
