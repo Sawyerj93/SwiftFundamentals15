ac830bcb861651848db8b5aa3e0133380ab926e9
Sawyerj93 <hrlyswyr@gmail.com>
2024-10-27 10:23:21 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/8. App Exercise - Tracking Different Types.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/8. App Exercise - Tracking Different Types.xcplaygroundpage/Contents.swift
@@ -10,8 +10,8 @@ var hasMetStepGoal: Bool = false
 
 //:  When you declared a constant for goal number of steps and a variable for current step count, you likely assigned each a value in the thousands. This can be difficult to read. Redeclare this constant and variable and, when assigning each a value in the thousands, format the number so that it is more readable.
 
-let stepsGoal = 10_000
-var Steps = 2_236
+let stepsGoal: Int = 10_000
+var Steps: Int = 2_236
 
 /*:
 [Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Inference and Required Values](@next)
