b1ea7a7ddeba62c55e19d3fdeb1227f185e2c113
Sawyerj93 <hrlyswyr@gmail.com>
2024-10-27 10:02:50 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/2. App Exercise - Step Goal.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/2. App Exercise - Step Goal.xcplaygroundpage/Contents.swift
@@ -8,6 +8,7 @@
 let goalSteps = 10000
 
 //:  Use two `print` functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of `goalSteps` by referencing your constant.
+
 print("Your step goal for the day is::)
 print(goalSteps)
       
