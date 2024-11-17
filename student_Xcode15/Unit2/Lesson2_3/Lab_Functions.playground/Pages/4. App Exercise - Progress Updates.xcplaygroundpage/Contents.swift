27dcbf8364383e1e5d3a5df4c87419d6fc484bb7
Sawyerj93 <hrlyswyr@gmail.com>
2024-11-16 22:29:57 -0500

    Update Contents.swift

--- student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_3/Lab_Functions.playground/Pages/4. App Exercise - Progress Updates.xcplaygroundpage/Contents.swift
@@ -9,10 +9,33 @@
  
  Call the function a number of times, passing in different values of `steps` and `goal`. Observe the printouts and make sure what is printed to the console is what you would expect for the parameters passsed in.
  */
+func progressUpdate(steps: Int, goal: Int) {
+    if steps < goal / 10 {
+        print("You're off to a good start.")
+    } else if steps < goal / 2 {
+        print("You're over halfway there!")
+    } else if steps < Int(Double(goal) * 0.9) {
+        print("You're almost there!")
+    } else {
+        print("You beat your goal!")
+    }
+}
 
+progressUpdate(steps: 5_000, goal: 15_000)
+progressUpdate(steps: 10_000, goal: 15_000)
 
 //:  Your fitness tracking app is going to help runners stay on pace to reach their goals. Write a function called pacing that takes four `Double` parameters called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. Your function should calculate whether or not the user is on pace to hit or beat `goalTime`. If yes, print "Keep it up!", otherwise print "You've got to push it just a bit harder!"
+func pacing(currentDistance: Double, totalDistance: Double, currentTime: Double, goalTime: Double) {
+    let pace = (currentDistance / totalDistance) > (currentTime / goalTime)
+    if pace {
+        print("Keep it up!")
+    } else {
+        print("You've got to push it just a bit harder!")
+    }
+}
 
+pacing(currentDistance: 8.4, totalDistance: 15, currentTime: 40, goalTime: 50)
+pacing(currentDistance: 8.4, totalDistance: 15, currentTime: 30, goalTime: 50)
 
 /*:
 [Previous](@previous)  |  page 4 of 6  |  [Next: Exercise - Return Values](@next)
